3fb0098d2db2abe822a862122bcf06d97cccb6ab
simonguo <simon.guo@hypers.com>
2018-05-28 15:51:59 +0800

    Update docs

--- docs/index.js
+++ docs/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Markdown } from 'react-markdown-reader';
 import { PageContainer } from 'rsuite-docs';
+import { Button } from 'rsuite';
+
 import Examples from './Examples';
 
 import './less/index.less';
@@ -17,36 +19,28 @@ class App extends React.Component {
         activeKey="CheckPicker"
         githubURL="https://github.com/rsuite/rsuite-checkpicker"
       >
-
-        <Markdown>
-          {require('../README.md')}
-        </Markdown>
+        <Markdown>{require('../README.md')}</Markdown>
 
         <h2>示例</h2>
 
         <Examples
-          dependencies={{ CheckPicker, data }}
+          dependencies={{ CheckPicker, Button, data }}
           list={[
             require('./md/default.md'),
             require('./md/group.md'),
             //require('./md/placement.md'),
             require('./md/custom.md'),
+            require('./md/custom-toggle.md'),
             require('./md/controlled.md'),
             require('./md/disabled.md'),
             require('./md/extra-footer.md')
           ]}
         />
 
-        <Markdown>
-          {require('./md/props.md')}
-        </Markdown>
-
+        <Markdown>{require('./md/props.md')}</Markdown>
       </PageContainer>
-
     );
   }
 }
 
-ReactDOM.render(<App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
